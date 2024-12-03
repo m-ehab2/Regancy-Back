6e246b0f47f08273de190f6463f55e6dc265d9f6
[200~mohamed-ezz1122~ <m7amedezz1122@gmail.com>
2024-12-03 13:54:20 +0200

    cors no

--- server.js
+++ server.js
@@ -29,20 +29,20 @@ db_connection()
 const app = express();
 const PORT = process.env.PORT || 3000;
 // app.use(cors())
-app.use(
-  // cors({
-  //   origin: "http://localhost:5173",
-  //   methods: ["GET", "POST", "DELETE", "PUT"],
-  //   allowedHeaders: [
-  //     "Content-Type",
-  //     "Authorization",
-  //     "Cache-Control",
-  //     "Expires",
-  //     "Pragma",
-  //   ],
-  //   credentials: true,
-  // })
-);
+// app.use(
+//   // cors({
+//   //   origin: "http://localhost:5173",
+//   //   methods: ["GET", "POST", "DELETE", "PUT"],
+//   //   allowedHeaders: [
+//   //     "Content-Type",
+//   //     "Authorization",
+//   //     "Cache-Control",
+//   //     "Expires",
+//   //     "Pragma",
+//   //   ],
+//   //   credentials: true,
+//   // })
+// );
 
 app.use(cookieParser());
 app.use(express.json());
